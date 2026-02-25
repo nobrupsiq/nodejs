44632b0c0be768198c84807eb372d240243995b1
bruno <bpires.siqueira@gmail.com>
2026-02-25 11:11:22 -0300

    feat: router

--- client.mjs
+++ client.mjs
@@ -1,13 +1,11 @@
-const response = await fetch("http://localhost:3000/produtos", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ username: "andre", password: "123456" }),
+const response = await fetch("http://localhost:3000/", {
+  method: "GET",
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
+  // body: JSON.stringify({ username: "andre", password: "123456" }),
 });
-
 // console.log(response);
+const body = await response.text();
 
-const body = await response.json();
-
-console.log(body.nome);
+console.log(body);
--- server.mjs
+++ server.mjs
@@ -1,9 +1,21 @@
 import { createServer } from "node:http";
+import { Router } from "./router.mjs";
+
+const router = new Router();
+router.get("/", (req, res) => {
+  res.end("Home");
+});
+
+router.get("/produto/notebook", (req, res) => {
+  res.end("Produtos - Notebook");
+});
+
+router.post("/produto", (req, res) => {
+  res.end("Notebook Post");
+});
 
 const server = createServer(async (req, res) => {
   const url = new URL(req.url, "http://localhost");
-
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   const chunks = [];
   for await (const chunk of req) {
@@ -11,28 +23,14 @@ const server = createServer(async (req, res) => {
   }
   const body = Buffer.concat(chunks).toString("utf-8");
 
-  if (req.method === "GET" && url.pathname === "/") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.end(`
-        <html>
-          <head>
-            <title>Mundo</title>
-          </head>
-          <body>
-           <h1>Olá mundo</h1>
-          </body>
-        </html>
-      `);
-  } else if (req.method === "POST" && url.pathname === "/produtos") {
-    res.statusCode = 201;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ nome: "Notebook" }));
+  const handler = router.find(req.method, url.pathname);
+  console.log(handler);
+  if (handler) {
+    handler(req, res);
   } else {
     res.statusCode = 404;
-    res.end("Página não encontra.");
+    res.end("Não encontrada");
   }
-  console.log(req.method);
 });
 
 server.listen(3000, () => {
